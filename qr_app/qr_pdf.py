f5d049009116788e6d2f42fd3eb05ce85de8ce13
TebaHa <Tchurlyaev.teba@yandex.com>
2020-01-07 15:53:03 +0300

    nu eto tochno last commit

--- qr_app/qr_pdf.py
+++ qr_app/qr_pdf.py
@@ -6,7 +6,7 @@
 #    By: zytrams <zytrams@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/11/09 09:51:59 by zytrams           #+#    #+#              #
-#    Updated: 2020/01/05 04:43:31 by zytrams          ###   ########.fr        #
+#    Updated: 2020/01/07 15:52:17 by zytrams          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -32,10 +32,19 @@ class PdfDecorator():
 
 	def put_label(self, x, y, string):
 		while string:
-			s = string[:30]
+			let_len = 0
+			last_space = 0
+			while let_len < 30:
+				if let_len == len(string):
+					last_space = let_len
+					break 
+				if string[let_len] == ' ':
+					last_space = let_len
+				let_len += 1
+			s = string[:last_space]
 			self.pdf.text(x, y, s)
 			y = y + 5
-			string = string[30:]
+			string = string[last_space + 1:]
 
 	def put_image(self, x, y, w, h, img):
 		img.save('uga.png', 'PNG')
