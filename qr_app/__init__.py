12ae2c0ec3f419fd3dcccb8e8762e4137b8b9575
Annara farring Halmon paege <ahalmon-@student.21-school.ru>
2020-01-24 00:11:32 +0300

    error fix

--- qr_app/__init__.py
+++ qr_app/__init__.py
@@ -20,14 +20,14 @@ from qr_app import qr_routes
 
 if 'QR_URL' in os.environ:
 	if 'QR_PORT' in os.environ:
-		app.run(os.environ['QR_URL'], port=os.environ['QR_PORT'])
+		qr_app.run(os.environ['QR_URL'], port=os.environ['QR_PORT'])
 	else:
-		app.run(os.environ['QR_URL'], port=8081)
+		qr_app.run(os.environ['QR_URL'], port=8081)
 else:
 	if 'URL' in os.environ:
 		if 'PORT' in os.environ:
-			app.run(os.environ['URL'], port=str(int(os.environ['PORT']) + 1))
+			qr_app.run(os.environ['URL'], port=str(int(os.environ['PORT']) + 1))
 		else:
-			app.run(os.environ['URL'], port=8081)
+			qr_app.run(os.environ['URL'], port=8081)
 	else:
-		app.run()
+		qr_app.run()
